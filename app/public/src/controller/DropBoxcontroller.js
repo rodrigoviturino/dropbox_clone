52d6265419b8d4e23331760fa01b2777904b70fa
Rodrigo Viturino <viturino_souza@outlook.com>
2020-08-03 02:36:10 -0300

    Criando método de adicionar class se o usuario selecionar os itens pressionando o SHIFT ou CTRL

--- app/public/src/controller/DropBoxcontroller.js
+++ app/public/src/controller/DropBoxcontroller.js
@@ -296,10 +296,14 @@ class DropBoxController {
       ${this.getFileIconView(file)}
       <div class="name text-center">${file.name}</div>
     `;
+
+    this.initEventsLi(li);
+
     return li;
   }
 
   readFiles() {
+    // Fazendo select no banco
     this.getFirebaseRef().on("value", (snapshot) => {
       // zerando ele para quando recarregar, ele receber valores atuais
       this.listFilesEl.innerHTML = "";
@@ -310,6 +314,43 @@ class DropBoxController {
 
         this.listFilesEl.appendChild(this.getFileView(data, key));
       });
+    });
+  }
+
+  initEventsLi(li) {
+    li.addEventListener("click", (event) => {
+      if (event.shiftKey) {
+        let firstLi = this.listFilesEl.querySelector(".selected");
+
+        if (firstLi) {
+          let indexStart;
+          let indexEnd;
+          let lis = li.parentElement.childNodes;
+
+          lis.forEach((el, index) => {
+            if (firstLi === el) indexStart = index;
+            if (li === el) indexEnd = index;
+          });
+
+          let index = [indexStart, indexEnd].sort();
+
+          lis.forEach((el, indx) => {
+            if (indx >= index[0] && indx <= index[1]) {
+              el.classList.add("selected");
+            }
+          });
+          // retornando para ele parar e não proseguir, se não vai executar o toggle lá em baixodo selected
+          return true;
+        }
+      }
+
+      if (!event.ctrlKey) {
+        this.listFilesEl.querySelectorAll("li.selected").forEach((el) => {
+          el.classList.remove("selected");
+        });
+      }
+
+      li.classList.toggle("selected");
     });
   }
 }
